33b09a7ba9f7395c1c3841d8a1a06423d1892056
Hiroki-Noguchi0001 <s19C1091DK@s.chibakoudai.jp>
2020-12-21 17:49:58 +0000

    add

--- led_control.c
+++ led_control.c
@@ -1,45 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-void output_number(char a)
+void output_number(int number)
 {
-		if(a == '0')
+		if(number == 0)
 		{
 			system("echo 0 > /dev/myled0");
 		}
-		else if(a == '1')
+		else if(number == 1)
 		{
 			system("echo 1 > /dev/myled0");
 		}
-		else if(a == '2')
+		else if(number == 2)
 		{
 			system("echo 2 > /dev/myled0");
 		}
-		else if(a == '3')
+		else if(number == 3)
 		{
 			system("echo 3 > /dev/myled0");
 		}
-		else if(a == '4')
+		else if(number == 4)
 		{
 			system("echo 4 > /dev/myled0");
 		}
-		else if(a == '5')
+		else if(number == 5)
 		{
 			system("echo 5 > /dev/myled0");
 		}
-		else if(a == '6')
+		else if(number == 6)
 		{
 			system("echo 6 > /dev/myled0");
 		}
-		else if(a == '7')
+		else if(number == 7)
 		{
 			system("echo 7 > /dev/myled0");
 		}
-		else if(a == '8')
+		else if(number == 8)
 		{
 			system("echo 8 > /dev/myled0");
 		}
-		else if(a == '9')
+		else if(number == 9)
 		{
 			system("echo 9 > /dev/myled0");
 		}
@@ -47,35 +48,46 @@ void output_number(char a)
 
 void random_number(void)
 {
+	srand((unsigned int)time(NULL));
 	system("echo r > /dev/myled0");
+	output_number(rand() % 10);
 }
 
 int main(void)
 {
-	char a;
+	int command;
+	system("clear");
 	printf("myledをロードします...\n");
 	system("sudo insmod myled.ko");
 	system("sudo chmod 666 /dev/myled0");
 	printf("出力したい数値を入力してください\n");
-	printf("0 ~ 9まで出力できます\n");
-	printf("cを入力すると終了します\n");
+	printf("*[0 ~ 9]まで出力できます\n");
+	printf("*[777]を入力する今日のラッキーナンバーが出力されます\n");
+	printf("*[123]を入力すると終了\n");
 
 	while(1)
 	{
-		scanf("%c", &a);
+		scanf("%d", &command);
 
-		output_number(a);
-		if(a == 'c')
+		if(command < 10)
+		{
+			output_number(command);
+		}
+
+		else if(command == 123)
 		{
 			break;
 		}
-		else if(a == 'r')
+		else if(command == 777)
 		{
 			random_number();
 		}
-
+		else
+		{
+			printf("その数字は対応しておりません\n");
+		}
 	}
-	
+
 	system("echo c > /dev/myled0");
 	system("sudo rmmod myled");
 	printf("myledをアンロードしました\n");
